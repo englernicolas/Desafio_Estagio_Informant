cc920071d37ed3daed7cd4c8ad689705a4d7c3c2
Nicolas Engler <nicolasengler@hotmail.com>
2019-06-13 21:53:51 -0300

    first commit

---
+++ src/Desafio.java
@@ -0,0 +1,89 @@
+import javax.swing.JOptionPane;
+
+public class Desafio {
+    public static void main(String[] args) {
+        while (true) {
+            String msg = JOptionPane.showInputDialog(null, "Digite uma palavra ou uma frase: ", "Verificador de palíndromos", JOptionPane.QUESTION_MESSAGE);
+            String msgSemEspacos = "";
+            String msgSemAcentosEspacos = "";
+
+            msgSemEspacos = tirarEspacos(msg);
+            msgSemEspacos = msgSemEspacos.toLowerCase();
+
+            msgSemAcentosEspacos = tirarAcentos(msgSemEspacos);
+
+            StringBuffer msgInvertida = InverterMsgFinal(msgSemAcentosEspacos);
+            String msgFinal = msgInvertida.toString();
+
+            verificadorPalindromo(msg, msgSemAcentosEspacos, msgFinal);
+
+            int continuar = JOptionPane.showConfirmDialog(null, "Deseja verificar outra palavra?", "Verificador de palíndromos", JOptionPane.YES_NO_OPTION);
+            if (continuar != JOptionPane.YES_OPTION) {
+                System.exit(0);
+            }
+        }
+    }
+
+    // método para tirar os espaços da String
+    private static String tirarEspacos(String msg) {
+        String msgSemEspaco = "";
+        for (int i = 0; i < msg.length(); i++) {
+            if (msg.charAt(i) != ' ') {
+                msgSemEspaco += msg.charAt(i);
+            }
+        }
+        return msgSemEspaco;
+    }
+
+    // método para tirar os acentos da String já sem os espaços
+    private static String tirarAcentos(String msgSemEspacos) {
+        String msgSemAcentos = "";
+        for (int i = 0; i < msgSemEspacos.length(); i++) {
+            char c = msgSemEspacos.charAt(i);
+            switch (c) {
+                case 'á':
+                case 'à':
+                case 'ã':
+                    c = 'a';
+                    break;
+                case 'é':
+                case 'ê':
+                    c = 'e';
+                    break;
+                case 'í':
+                    c = 'i';
+                    break;
+                case 'ó':
+                case 'õ':
+                case 'ô':
+                    c = 'o';
+                    break;
+                case 'ú':
+                    c = 'u';
+                    break;
+                case 'ç':
+                    c = 'c';
+                    break;
+            }
+            msgSemAcentos += c;
+        }
+        return msgSemAcentos;
+    }
+
+    // método para inverter a String já sem acentos ou espaços
+    private static StringBuffer InverterMsgFinal(String msgSemAcentosEspacos) {
+        StringBuffer msgFinalS = new StringBuffer(msgSemAcentosEspacos);
+        msgFinalS.reverse();
+        return msgFinalS;
+    }
+
+    // método para verificar se a String final é ou não um palíndromo
+    private static void verificadorPalindromo(String msg, String msgSemAcentosEspacos, String msgFinal) {
+        if (msgSemAcentosEspacos.equals(msgFinal)) {
+            JOptionPane.showMessageDialog(null, "'" + msg + "' é palíndromo!", "Verificador de palíndromos", JOptionPane.INFORMATION_MESSAGE);
+        } else {
+            JOptionPane.showMessageDialog(null, "'" + msg + "' não é palíndromo!", "Verificador de palíndromos", JOptionPane.INFORMATION_MESSAGE);
+        }
+    }
+
+}
